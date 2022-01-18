f1fa114fb8f233c97c91d9e7793db28f6e39e583
boomhaa <90379111+boomhaa@users.noreply.github.com>
2022-01-18 14:30:18 +0300

    gg

--- main.py
+++ main.py
@@ -1,13 +1,34 @@
 import requests
-import json
+
+pokemons={}
+
+class Pokemon:
+    def __init__(self,json):
+        self.name=json["name"]
+        self.height = json["height"]
+        self.abilities=[]
+        for i in range(len(json["abilities"])):
+            self.abilities.append(json["abilities"][i]["ability"]["name"])
+
+
+    def check_status(self):
+        r = requests.get("https://pokeapi.co/api/v2/pokemon/" + self.name)
+        if r.status_code==200:
+            return True
+        return False
+    def get(self):
+        print("name:",self.name)
+        print("height:",self.height)
+        print("abilities:",", ".join(self.abilities))
 
 while True:
-    command = input()
-    if command == "add":
-        req = requests.get("https://aws.random.cat/meow")
-        r=requests.get(json.loads(req.text)["file"])
-        content=r.content
-        img=open("img.jpg","wb")
-        img.write(content)
-        img.close()
-        print("nice")
+    command=input()
+    if command=="get":
+        name_pokemon=input("Введите имя: ")
+        if pokemons.get(name_pokemon):
+            pokemons.get(name_pokemon).get()
+        else:
+            r = requests.get("https://pokeapi.co/api/v2/pokemon/" + name_pokemon)
+            r=r.json()
+            pokemons[name_pokemon]=Pokemon(r)
+            pokemons[name_pokemon].get()
